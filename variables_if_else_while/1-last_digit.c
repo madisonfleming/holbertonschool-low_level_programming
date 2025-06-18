3f4193fcc7869418e992cba29228bf5d58dcdae2
“madisonfleming” <maddyyfleming@gmail.com>
2025-06-18 11:04:14 +0000

    fingerscrossed

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -18,19 +18,19 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	last_digit = n % 10;
 
-	printf("Last digit of %d is", n);
+	printf("Last digit of %d is %d", n, last_digit);
 
 	if (last_digit > 5)
 	{
-		printf("%d and is greater than 5\n", last_digit);
+		printf("and is greater than 5\n");
 	}
 	else if (last_digit == 0)
 	{
-		printf("%d and is 0\n", last_digit);
+		printf("and is 0\n");
 	}
-	else if (n < 6 || n != 0)
+	else if (last_digit < 6)
 	{
-		printf("%d and is less than 6 and not 0\n", last_digit);
+		printf("and is less than 6 and not 0\n");
 	}
 	return (0);
 }
