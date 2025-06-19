2732336bbdbdc18ad13fcd3e780c086f6da4cb78
“madisonfleming” <maddyyfleming@gmail.com>
2025-06-19 23:42:26 +0000

    file3!!

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - entry point
+ * Decription: for loop and putchar
+ * Return: Success (0)
+ */
+
+int main(void)
+{
+	int i;
+
+for (i = 97; i <= 122; i++)
+{
+	putchar(i);
+}
+for (i = 65; i <= 90; i++)
+{
+	putchar(i);
+}
+	putchar('\n');
+
+return (0);
+}
