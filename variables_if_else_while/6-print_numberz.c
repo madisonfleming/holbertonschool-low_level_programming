75185c897703c83dd9f7405b682cd67a26063bbc
“madisonfleming” <maddyyfleming@gmail.com>
2025-06-20 02:23:18 +0000

    file6check

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: printing numberz
+ * Return: Success (0)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 57; num <= 48; num--)
+	{
+		putchar(num);
+	}
+		putchar('\n');
+return (0);
+}
