5ee1e18f16a6a434206947a19135794ceacab064
“madisonfleming” <maddyyfleming@gmail.com>
2025-06-20 02:06:44 +0000

    file5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: print single digit numbers
+ * Return: Success (0)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		printf("%d", num);
+	}
+		printf("\n");
+
+return (0);
+}
