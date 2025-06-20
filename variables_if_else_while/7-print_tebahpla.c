b3d71defbe306e1f5154dbc29a2a315dc28d82e2
“madisonfleming” <maddyyfleming@gmail.com>
2025-06-20 02:38:11 +0000

    file7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - entry point
+ * Decription: for loop and putchar
+ * Return: Success (0)
+ */
+
+int main(void)
+{
+	int i;
+
+for (i = 122; i >= 97; i--)
+{
+	putchar(i);
+}
+	putchar('\n');
+
+return (0);
+}
