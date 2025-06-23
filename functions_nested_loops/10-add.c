07f99f6db1fc0f27c2b87e06be3c91bd6db7e678
“madisonfleming” <maddyyfleming@gmail.com>
2025-06-23 04:45:52 +0000

    file9

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - add two integers and return the result
+ * @num1: first integer
+ * @num2: second integer
+ * Return: sum of num1 and num2
+ */
+int add(int num1, int num2)
+{
+	int result;
+
+	result = num1 + num2;
+
+return (result);
+}
