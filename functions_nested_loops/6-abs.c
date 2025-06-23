6d7f6d4be32bb0b7245974456ec8cd78cfe572f5
“madisonfleming” <maddyyfleming@gmail.com>
2025-06-23 01:00:39 +0000

    file6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - compute the absolute value of an integer
+ * @n: the integer to check
+ * Description: _abs is the absolute value of int
+ * Return: absolute value of n
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+return (0);
+}
