23b3f2184cbfffad9896c2c16852ad1713445c04
“madisonfleming” <maddyyfleming@gmail.com>
2025-06-24 06:53:43 +0000

    file9

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: print from 1 to 100
+ * printf: multiples of three print Fizz and five print Buzz
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 1; num <= 100; num++)
+	{
+	if (num % 3 == 0 && num % 5 == 0)
+		{
+		printf("Fizzbuzz ");
+		}
+	else if (num % 3 == 0)
+		{
+		printf("Fizz ");
+		}
+	else if (num % 5 == 0)
+		{
+		printf("Buzz ");
+		}
+	else
+	{
+		printf("%d ", num);
+	}
+	}
+	printf("\n");
+	return (0);
+}
