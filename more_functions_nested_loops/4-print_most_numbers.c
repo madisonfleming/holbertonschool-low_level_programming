46d576e018e6b728df8c355436534bfc20826d12
“madisonfleming” <maddyyfleming@gmail.com>
2025-06-24 02:45:33 +0000

    file4havingbreaksave

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_most_numbers - print number from 0 to 9, without 2 and 4
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+	{
+		if (num != 2 && num != 4)
+		{
+			_putchar('0' + num);
+			num = num + 1;
+		}
+		_putchar('\n');
+	}
+}
