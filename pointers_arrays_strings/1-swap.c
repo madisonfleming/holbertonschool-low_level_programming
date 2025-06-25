3ba63521097488e4b3f97f49b53c0beae7cb5f9b
“madisonfleming” <maddyyfleming@gmail.com>
2025-06-25 04:20:54 +0000

    file1!

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * swap_int - swapping the value of two integers
+ * @a: integer to swap
+ * @b: integer to swap
+ * return: void
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
