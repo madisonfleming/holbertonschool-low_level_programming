4934698b27ff3b581db784b84d00d20e739d58d0
“madisonfleming” <maddyyfleming@gmail.com>
2025-07-04 09:27:52 +0000

    file3agaaaaaain

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -32,12 +32,9 @@ void print_number(int n)
 	_putchar('-');
 	n = -n;
 	}
-	else
+	if (n / 10)
 	{
-		if (n / 10)
-		{
-		print_number(n / 10);
-		}
+	print_number(n / 10);
 	}
 	_putchar((n % 10) + '0');
 }
