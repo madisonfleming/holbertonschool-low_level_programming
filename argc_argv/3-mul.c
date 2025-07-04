0e9cf4e77d44546f0d482c5cf48b71e480d41e67
“madisonfleming” <maddyyfleming@gmail.com>
2025-07-04 09:21:48 +0000

    file3again

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,45 +1,75 @@
 #include "main.h"
 /**
-* main - multiply two numbers
-* @argc: number of arguments
-* @argv: pointer to string of arguments
-* Return: Result otherwise return error if two arguments not received
-*/
+ * _atoi - convert string to int
+ * @s: input string
+ * Return: converted integer
+ */
+int _atoi(char *s)
+{
+	int sign = 1, result = 0, i = 0;
+
+	if (s[0] == '-')
+	{
+	sign = -1;
+	i++;
+	}
+	while (s[i] != '\0')
+	{
+	result = result * 10 + (s[i] - '0');
+	i++;
+	}
+
+	return (sign * result);
+}
+/**
+ * print_number - print an integer using _putchar through recursion
+ * @n: the number to print
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+	_putchar('-');
+	n = -n;
+	}
+	else
+	{
+		if (n / 10)
+		{
+		print_number(n / 10);
+		}
+	}
+	_putchar((n % 10) + '0');
+}
+/**
+ * main - multipliy two numbers and print result through recursion
+ * @argc: argument count
+ * @argv: pointer to string of arguments
+ * Return: Success or error if 3 arguments not passed
+ */
 int main(int argc, char *argv[])
 {
+	int i = 0;
 	int num1;
 	int num2;
-	int sum;
-	int digits[15];
-	int i = 0;
 
 	if (argc != 3)
 	{
-		_puts("Error\n");
+		char *error = "Error\n";
+
+		while (error[i])
+		{
+			_putchar(error[i]);
+			i++;
+		}
 		return (1);
 	}
 
 	num1 = _atoi(argv[1]);
 	num2 = _atoi(argv[2]);
-	sum = num1 * num2;
 
-	if (sum < 0)
-	{
-		_putchar('-');
-		sum = sum * -1;
-	}
-	while (sum > 0)
-	{
-		digits[i] = sum % 10;
-		sum = sum / 10;
-		i = i + 1;
-	}
-	i = i - 1;
-	while (i >= 0)
-	{
-		_putchar(digits[i] + '0');
-		i = i - 1;
-	}
-		_putchar('\n');
-return (0);
+	print_number(num1 * num2);
+	_putchar('\n');
+
+	return (0);
 }
