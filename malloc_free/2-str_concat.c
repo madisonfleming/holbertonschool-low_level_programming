f00c2f3766651e9f3bc9087aaf17626f4414b9e9
“madisonfleming” <maddyyfleming@gmail.com>
2025-07-10 05:17:07 +0000

    again

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -15,25 +15,21 @@ char *str_concat(char *s1, char *s2)
 	int len2 = 0;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
 
 	while (s1[len1] != '\0')
-	{
 		len1++;
-	}
+
 	while (s2[len2] != '\0')
-	{
 		len2++;
-	}
 
 	s = malloc(sizeof(char) * (len1 + len2 + 1));
+
+	if (s == NULL)
+		return (NULL);
 
 	while (i < len1)
 	{
