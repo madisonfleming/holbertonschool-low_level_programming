7564509e42a8a26c5bf2c7936665489b9275cf02
“madisonfleming” <maddyyfleming@gmail.com>
2025-07-10 04:34:14 +0000

    file2

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include "stdlib.h"
+/**
+ * str_concat - concatenate two strings with malloc
+ * @s1: the first string
+ * @s2: the second string
+ * Return: pointer to new string otherwise Null on failure or empty string
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *s;
+	int i = 0;
+
+	if (s1 == NULL && s2 == NULL)
+	{
+		return (NULL);
+	}
+
+	s = malloc(i + 1);
+
+	while (*s1 != '\0')
+	{
+		s[i] = *s1;
+		s1++;
+		i++;
+	}
+
+	while (*s2 != '\0')
+	{
+		s[i] = *s2;
+		s2++;
+		i++;
+	}
+	s[i] = '\0';
+return (s);
+}
--- malloc_free/main.h
+++ malloc_free/main.h
@@ -43,5 +43,6 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
 
 #endif
