8d647c8681726c140aee628b45817e42b27b2cb8
“madisonfleming” <maddyyfleming@gmail.com>
2025-07-10 06:51:50 +0000

    file3

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include "stdlib.h"
+/**
+ * alloc_grid - a pointer to a 2 dimensional array of ints
+ * @width: width of the grid
+ * @height: height of the grid
+ * Return: pointer to 2 dimensional array or NULL if error
+ */
+int **alloc_grid(int width, int height)
+{
+	int x = 0;
+	int y = 0;
+	int **grid;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	grid = malloc(height * sizeof(int *));
+
+	if (grid == 0)
+	{
+		free(grid);
+		return (NULL);
+	}
+	while (y < height)
+	{
+		grid[y] = malloc(width * sizeof(int));
+		if (grid[y] == 0)
+		{
+			while (y <= 0)
+			{
+				free(grid[y]);
+				y = y - 1;
+			}
+			free(grid);
+			return (NULL);
+		}
+		while (x < width)
+		{
+			grid[y][x] = 0;
+			x = x + 1;
+		}
+		y = y + 1;
+		x = 0;
+	}
+return (grid);
+}
