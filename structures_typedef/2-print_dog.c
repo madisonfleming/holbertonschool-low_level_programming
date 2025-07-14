547fa8b1ae7e8b231b6f71e17c77444a8ccd43e5
“madisonfleming” <maddyyfleming@gmail.com>
2025-07-14 04:44:50 +0000

    nullnotwork

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -11,21 +11,14 @@ void print_dog(struct dog *d)
 		return;
 
 	if (d->name == NULL)
-	{
 		printf("Name: (nil)");
-	}
 	else
-	{
 		printf("Name: %s\n", d->name);
-	}
+
 	printf("Age: %f\n", d->age);
 
 	if (d->owner == NULL)
-	{
-		printf("(nil)");
-	}
+		printf("Owner: (nil)\n");
 	else
-	{
 		printf("Owner: %s\n", d->owner);
-	}
 }
