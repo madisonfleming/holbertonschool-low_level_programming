547c87b00b68b0648c312cf66ab644b5a572c5c1
“madisonfleming” <maddyyfleming@gmail.com>
2025-07-15 02:45:14 +0000

    _putagain

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -2,18 +2,12 @@
 #define FUNCTION_POINTERS
 
 /**
- * _putchar - does the same as putchar()
  * return: Always 0.
  */
 
 #include <unistd.h>
 
-int _putchar(char c)
-{
-        return (write(1, &c, 1));
-}
-
-int _putchar(int c);
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
