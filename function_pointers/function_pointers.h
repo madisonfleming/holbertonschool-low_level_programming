1654aa64e3aa30d5c549e41e4337baca68bd5786
“madisonfleming” <maddyyfleming@gmail.com>
2025-07-15 02:39:12 +0000

    _putchar

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -6,6 +6,13 @@
  * return: Always 0.
  */
 
+#include <unistd.h>
+
+int _putchar(char c)
+{
+        return (write(1, &c, 1));
+}
+
 int _putchar(int c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
