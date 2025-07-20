24a4af7ada2416c65f145d42603b40f1169dc77a
“madisonfleming” <maddyyfleming@gmail.com>
2025-07-20 07:46:36 +0000

    file3

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,86 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_char - print char
+ * @args: argument to print
+ */
+void print_char(va_list *args)
+{
+	printf("%c", va_arg(*args, int));
+}
+
+/**
+* print_int - print int
+* @args: argument to print
+*/
+void print_int(va_list *args)
+{
+	printf("%d", va_arg(*args, int));
+}
+
+/**
+ * print_float - print float
+ * @args: argument to print
+ */
+void print_float(va_list *args)
+{
+	printf("%f", va_arg(*args, double));
+}
+
+/**
+ * print_string - print string
+ * @args: argument to print
+ */
+void print_string(va_list *args)
+{
+	char *str = va_arg(*args, char *);
+
+	if (str == NULL)
+		printf("(nil)");
+	else
+		printf("%s", str);
+}
+
+/**
+ * print_all - print all arguments
+ * @format: list of all arguments
+ */
+void print_all(const char * const format, ...)
+{
+	op_t ops[] = {
+	{'c', print_char},
+	{'i', print_int},
+	{'f', print_float},
+	{'s', print_string}
+	};
+
+	va_list list;
+	int i = 0;
+	int j = 0;
+	int printed_one = 0;
+	int match;
+
+	va_start(list, format);
+	while (format != NULL && format[i] != '\0')
+	{
+		match = 0;
+		while (j < 4)
+		{
+			if (ops[j].type == format[i] && match == 0)
+			{
+				if (printed_one)
+					printf(", ");
+				ops[j].f(&list);
+			printed_one = 1;
+			match = 1;
+			}
+			j++;
+		}
+		j = 0;
+		i++;
+	}
+	va_end(list);
+	printf("\n");
+}
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -10,9 +10,23 @@
  */
 
 #include <unistd.h>
+#include <stdarg.h>
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+/**
+ * struct op - struct for format value
+ * Description: find the format value
+ * @f: function pointer
+ * @type: type of value
+ */
+typedef struct op
+{
+	char type;
+	void (*f)(va_list *);
+} op_t;
 
 #endif
