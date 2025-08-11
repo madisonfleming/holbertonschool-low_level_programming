60c5fc2dfc347aceaa54c34f5105ee26b90e5dc8
madisonfleming <maddyyfleming@gmail.com>
2025-08-11 22:48:29 +0000

    freespace

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -12,20 +12,30 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new_node;
-	unsigned int index;
+	unsigned long int index;
 
 	index = key_index((const unsigned char *)key, ht->size);
 
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
+	{
+		free(new_node);
+		return (0);
+	}
+	new_node->key = strdup(key);
+	if (new_node->key == NULL)
 		return (0);
 
-	else
+	new_node->value = strdup(value);
+	if (new_node->value == NULL)
 	{
-		new_node->key = strdup(key);
-		new_node->value = strdup(value);
-		new_node->next = NULL;
+		free(new_node->key);
+		free(new_node);
+		return (0);
 	}
+
+	new_node->next = NULL;
+
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 return (1);
