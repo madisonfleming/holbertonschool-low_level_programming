76ced89425c7e0249473c5eaa12f0c5094e0d07b
madisonfleming <maddyyfleming@gmail.com>
2025-08-28 01:46:49 +0000

    fix4+5

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -11,6 +11,9 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *current;
 
+	if (ht == NULL || key == NULL)
+		return (NULL);
+
 	index = key_index((const unsigned char *)key, ht->size);
 	current = ht->array[index];
 
--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -10,17 +10,22 @@ void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i = 0;
 	hash_node_t *current;
+	int first = 1;
 
 	if (ht == NULL)
 		return;
 
 	printf("{");
 	while (i < ht->size)
-	{	
+	{
 		current = ht->array[i];
 		while (current != NULL)
 		{
+			if (first != 1)
+				printf(", ");
 			printf("'%s': '%s'", current->key, current->value);
+			first = 0;
+
 			current = current->next;
 		}
 		i++;
