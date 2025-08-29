2d7a9cdcc8310ea559727a68bea461b913b09590
madisonfleming <maddyyfleming@gmail.com>
2025-08-29 06:07:23 +0000

    file4

---
+++ file_io/3-cp.c
@@ -0,0 +1,73 @@
+#include "main.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define BUFFER_SIZE 1024
+
+void close_fd(int fd);
+
+/**
+ * main - copy contents of a file to another file
+ * @ac: argument count
+ * @av: arguments
+ * Return: 0 if Success otherwise exit code if fail
+ */
+int main(int ac, char *av[])
+{
+	int fd_from, fd_to, r, w;
+	char buffer[BUFFER_SIZE];
+
+	if (ac != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	fd_from = open(av[1], O_RDONLY);
+	if (fd_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+	fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		close(fd_from);
+		exit(99);
+	}
+	while ((r = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	{
+		w = write(fd_to, buffer, r);
+		if (w != r)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+			close_fd(fd_from);
+			close_fd(fd_to);
+			exit(99);
+		}
+	}
+	if (r == -1)
+	{
+		dprintf(STDERR_FILENO, "ERROR: Can't read from file %s\n", av[1]);
+		close_fd(fd_from);
+		close_fd(fd_to);
+		exit(98);
+	}
+	close_fd(fd_from);
+	close_fd(fd_to);
+	return (0);
+}
+/**
+ * close_fd - close a file desciptor with error handling
+ * @fd: file descriptor
+ */
+void close_fd(int fd)
+{
+	if (close(fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
