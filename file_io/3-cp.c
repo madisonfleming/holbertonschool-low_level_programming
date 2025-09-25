60d03e410b5ab2b365a87aaa8e1c8a3ee27c2a5c
madisonfleming <maddyyfleming@gmail.com>
2025-09-25 18:54:32 +1000

    bett

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -14,8 +14,8 @@
  */
 void error_exit(int code, const char *format, const char *arg)
 {
-	dprintf(STDERR_FILENO, format, arg);
-	exit(code);
+        dprintf(STDERR_FILENO, format, arg);
+        exit(code);
 }
 
 /**
@@ -24,12 +24,42 @@ void error_exit(int code, const char *format, const char *arg)
 */
 void close_fd(int fd)
 {
-	if (close(fd) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-		exit(100);
-	}
+        if (close(fd) == -1)
+        {
+                dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+                exit(100);
+        }
 }
+
+/**
+ * copy_file - copy contents from fd_from to fd_to
+ * @fd_from: file from
+ * @fd_to: file to
+ * @file_from: pointer to file from
+ * @file_to: pointer to file to
+ * Return: void, exits on error
+ */
+void copy_file(int fd_from, int fd_to, char *file_from, char *file_to)
+{
+        char buffer[BUF_SIZE];
+        ssize_t r_bytes, w_bytes;
+
+        r_bytes = read(fd_from, buffer, BUF_SIZE);
+        if (r_bytes == -1)
+                error_exit(98, "Error: Can't read from file %s\n", file_from);
+
+        while (r_bytes > 0)
+        {
+                w_bytes = write(fd_to, buffer, r_bytes);
+                if (w_bytes != r_bytes)
+                        error_exit(99, "Error: Can't write to %s\n", file_to);
+
+                r_bytes = read(fd_from, buffer, BUF_SIZE);
+                if (r_bytes == -1)
+                        error_exit(98, "Error: Can't read from file %s\n", file_from);
+        }
+}
+
 /**
  * main - Copies content of file to another
  *
@@ -41,51 +71,25 @@ void close_fd(int fd)
  */
 int main(int argc, char **argv)
 {
-	int fd_from, fd_to, r_bytes, w_bytes;
-	char buffer[BUF_SIZE];
+        int fd_from, fd_to;
 
-	if (argc != 3)
-		error_exit(97, "Usage: cp file_from file_to\n", "");
+        if (argc != 3)
+                error_exit(97, "Usage: cp file_from file_to\n", "");
 
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
-		error_exit(98, "Error: Can't read from file %s\n", argv[1]);
+        fd_from = open(argv[1], O_RDONLY);
+        if (fd_from == -1)
+                error_exit(98, "Error: Can't read from file %s\n", argv[1]);
 
-	r_bytes = read(fd_from, buffer, BUF_SIZE);
-	if (r_bytes == -1)
-	{
-		close(fd_from);
-		error_exit(98, "Error: Can't read from file %s\n", argv[1]);
-	}
+        fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+        if (fd_to == -1)
+        {
+                close(fd_from);
+                error_exit(99, "Error: Can't write to %s\n", argv[2]);
+        }
 
-	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (fd_to == -1)
-	{
-		close(fd_from);
-		error_exit(99, "Error: Can't write to %s\n", argv[2]);
-	}
+        copy_file(fd_from, fd_to, argv[1], argv[2]);
 
-	while (r_bytes > 0)
-	{
-		w_bytes = write(fd_to, buffer, r_bytes);
-		if (w_bytes != r_bytes)
-		{
-			close(fd_from);
-			close(fd_to);
-			error_exit(99, "Error: Can't write to %s\n", argv[2]);
-		}
-
-		r_bytes = read(fd_from, buffer, BUF_SIZE);
-		if (r_bytes == -1)
-		{
-			close(fd_from);
-			close(fd_to);
-			error_exit(98, "Error: Can't read from file %s\n", argv[1]);
-		}
-	}
-
-	close_fd(fd_from);
-	close_fd(fd_to);
-
-	return (0);
+        close_fd(fd_from);
+        close_fd(fd_to);
+return (0);
 }
