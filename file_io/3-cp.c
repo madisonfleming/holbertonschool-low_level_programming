a7ebad3de6275390b3b42765c8a97d5bf005a643
madisonfleming <maddyyfleming@gmail.com>
2025-09-25 18:23:04 +1000

    again

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -20,36 +20,57 @@ int main(int argc, char **argv)
 	char buffer[1024];
 
 	if (argc != 3)
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
 	fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
+
 	fd2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd2 == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
-
-	filecheck = read(fd, buffer, 1024);
-	while (filecheck > 0)
 	{
-		if (write(fd2, buffer, filecheck) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		close(fd);
+		exit(99);
+	}
+
+	while ((r_bytes = read(fd, buffer, 1024)) > 0)
+	{
+		w_bytes = write(fd2, buffer, r_bytes);
+		if (w_bytes != r_bytes)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			close(fd);
+			close(fd2);
 			exit(99);
 		}
-		filecheck = read(fd, buffer, 1024);
 	}
-	if (filecheck == -1)
+
+	if (r_bytes == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		close(fd);
+		close(fd2);
 		exit(98);
 	}
+
 	if (close(fd) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd), exit(100);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+
 	if (close(fd2) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2), exit(100);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
+		exit(100);
+	}
 
 	return (0);
 }
